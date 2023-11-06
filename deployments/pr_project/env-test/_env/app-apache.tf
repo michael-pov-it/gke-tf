09f3a13611b28474cf8be524b8b02c75ff5e62e7
Mike Gordievsky <mike.gordievsky@gmail.com>
2023-11-06 17:36:32 +0100

    test cluster deploy 3.5

--- deployments/pr_project/env-test/_env/app-apache.tf
+++ deployments/pr_project/env-test/_env/app-apache.tf
@@ -1,42 +1,42 @@
-resource "kubernetes_pod" "apache-example" {
-  metadata {
-    name = "apache-test-pod"
+# resource "kubernetes_pod" "apache-example" {
+#   metadata {
+#     name = "apache-test-pod"
 
-    labels = {
-      maintained_by = "terraform"
-      app           = "apache-example"
-    }
-  }
+#     labels = {
+#       maintained_by = "terraform"
+#       app           = "apache-example"
+#     }
+#   }
 
-  spec {
-    container {
-      image = "httpd:latest"
-      name  = "apache-example"
-    }
-  }
+#   spec {
+#     container {
+#       image = "httpd:latest"
+#       name  = "apache-example"
+#     }
+#   }
 
-  depends_on = [module.gke]
-}
+#   depends_on = [module.gke]
+# }
 
-resource "kubernetes_service" "apache-example" {
-  metadata {
-    name = "apache-test-svc"
-  }
+# resource "kubernetes_service" "apache-example" {
+#   metadata {
+#     name = "apache-test-svc"
+#   }
 
-  spec {
-    selector = {
-      app = kubernetes_pod.apache-example.metadata[0].labels.app
-    }
+#   spec {
+#     selector = {
+#       app = kubernetes_pod.apache-example.metadata[0].labels.app
+#     }
 
-    session_affinity = "ClientIP"
+#     session_affinity = "ClientIP"
 
-    port {
-      port        = 80
-      target_port = 80
-    }
+#     port {
+#       port        = 80
+#       target_port = 80
+#     }
 
-    type = "LoadBalancer"
-  }
+#     type = "LoadBalancer"
+#   }
 
-  depends_on = [module.gke]
-}
+#   depends_on = [module.gke]
+# }
