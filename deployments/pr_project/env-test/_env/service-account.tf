580bf2f3b1a22f6deca9d4c36ce1bfdbee809b9d
Mike Gordievsky <mike.gordievsky@gmail.com>
2023-11-06 17:39:28 +0100

    test cluster deploy 3.6

--- deployments/pr_project/env-test/_env/service-account.tf
+++ deployments/pr_project/env-test/_env/service-account.tf
@@ -1,10 +1,10 @@
-resource "google_service_account" "mike-name" {
-  account_id = "mike-name"
-  display_name = "SA"
-}
+# resource "google_service_account" "mike-name" {
+#   account_id = "mike-name"
+#   display_name = "SA"
+# }
 
-resource "google_project_iam_member" "firestore_owner_binding" {
+resource "google_project_iam_member" "mike-owner" {
   project = "gke-test-tf1"
   role    = "roles/owner"
-  member  = "serviceAccount:${google_service_account.mike-name.email}"
+  member  = "serviceAccount:mike-test-gke1@gke-test-tf1.iam.gserviceaccount.com"// "serviceAccount:${google_service_account.mike-name.email}"
 }
