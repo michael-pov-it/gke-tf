16fa9394205f25fa945b3809333e24fc148c50de
Mike Gordievsky <mike.gordievsky@gmail.com>
2023-11-06 17:34:36 +0100

    test cluster deploy 3.4

--- deployments/pr_project/env-test/_env/gcp-network.tf
+++ deployments/pr_project/env-test/_env/gcp-network.tf
@@ -1,6 +1,5 @@
 module "gcp-network" {
   source       = "terraform-google-modules/network/google"
-  # version      = "6.0.0"
   project_id   = var.project_id
   network_name = "${var.network}-${var.env_name}"
 
