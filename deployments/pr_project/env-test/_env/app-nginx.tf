a25a59fc8df3a7aab3ad2df8fac765bfdccfbf54
Mike Gordievsky <mike.gordievsky@gmail.com>
2023-11-07 18:24:58 +0100

    deploy nginx and apache with svcs 1

--- deployments/pr_project/env-test/_env/app-nginx.tf
+++ deployments/pr_project/env-test/_env/app-nginx.tf
@@ -1,42 +1,42 @@
-# resource "kubernetes_pod" "nginx-example" {
-#   metadata {
-#     name = "nginx-test-pod"
+resource "kubernetes_pod" "nginx-example" {
+  metadata {
+    name = "nginx-test-pod"
 
-#     labels = {
-#       maintained_by = "terraform"
-#       app           = "nginx-example"
-#     }
-#   }
+    labels = {
+      maintained_by = "terraform"
+      app           = "nginx-example"
+    }
+  }
 
-#   spec {
-#     container {
-#       image = "nginx:1.25.3"
-#       name  = "nginx-example"
-#     }
-#   }
+  spec {
+    container {
+      image = "nginx:1.25.3"
+      name  = "nginx-example"
+    }
+  }
 
-#   depends_on = [module.gke]
-# }
+  depends_on = [module.gke]
+}
 
-# resource "kubernetes_service" "nginx-example" {
-#   metadata {
-#     name = "nginx-test-svc"
-#   }
+resource "kubernetes_service" "nginx-example" {
+  metadata {
+    name = "nginx-test-svc"
+  }
 
-#   spec {
-#     selector = {
-#       app = kubernetes_pod.nginx-example.metadata[0].labels.app
-#     }
+  spec {
+    selector = {
+      app = kubernetes_pod.nginx-example.metadata[0].labels.app
+    }
 
-#     session_affinity = "ClientIP"
+    session_affinity = "ClientIP"
 
-#     port {
-#       port        = 8080
-#       target_port = 80
-#     }
+    port {
+      port        = 8080
+      target_port = 80
+    }
 
-#     type = "LoadBalancer"
-#   }
+    type = "LoadBalancer"
+  }
 
-#   depends_on = [module.gke]
-# }
+  depends_on = [module.gke]
+}
