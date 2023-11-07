1cc2bf92a07483918d448d1380d7321bca4e21c8
Mike Gordievsky <mike.gordievsky@gmail.com>
2023-11-07 10:40:08 +0100

    test cluster deploy 3.8

--- deployments/pr_project/env-test/_env/service-account.tf
+++ deployments/pr_project/env-test/_env/service-account.tf
@@ -1,10 +1,25 @@
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/editor"
+
+    members = [
+      "user:witty87@google.com",
+    ]
+  }
+}
+
+resource "google_project_iam_policy" "project" {
+  project     = "gke-test-tf1"
+  policy_data = data.google_iam_policy.admin.policy_data
+}
+
 # resource "google_service_account" "mike-name" {
 #   account_id = "mike-name"
 #   display_name = "SA"
 # }
 
-resource "google_project_iam_member" "mike-owner" {
-  project = "gke-test-tf1"
-  role    = "roles/editor"
-  member  = "serviceAccount:mike-test-gke1@gke-test-tf1.iam.gserviceaccount.com"// "serviceAccount:${google_service_account.mike-name.email}"
-}
+# resource "google_project_iam_member" "mike-owner" {
+#   project = "gke-test-tf1"
+#   role    = "roles/editor"
+#   member  = "serviceAccount:mike-test-gke1@gke-test-tf1.iam.gserviceaccount.com"// "serviceAccount:${google_service_account.mike-name.email}"
+# }
