976b80db30d87f1739c4a2300c3df2359385cc69
Mike Gordievsky <mike.gordievsky@gmail.com>
2023-11-07 10:25:20 +0100

    test cluster deploy 3.7

--- deployments/pr_project/env-test/_env/service-account.tf
+++ deployments/pr_project/env-test/_env/service-account.tf
@@ -5,6 +5,6 @@
 
 resource "google_project_iam_member" "mike-owner" {
   project = "gke-test-tf1"
-  role    = "roles/owner"
+  role    = "roles/editor"
   member  = "serviceAccount:mike-test-gke1@gke-test-tf1.iam.gserviceaccount.com"// "serviceAccount:${google_service_account.mike-name.email}"
 }
