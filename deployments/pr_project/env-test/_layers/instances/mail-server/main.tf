15aca5f7f2120df2698e8f6d9699762b91192830
Mike Gordievsky <mike.gordievsky@gmail.com>
2023-11-15 11:09:15 +0100

    version 1.0.3

--- deployments/pr_project/env-test/_layers/instances/mail-server/main.tf
+++ deployments/pr_project/env-test/_layers/instances/mail-server/main.tf
@@ -8,7 +8,7 @@ resource "google_compute_instance" "mail-server" {
   machine_type = "n2-standard-2"
   zone         = var.isntance_zone
 
-  tags = ["mail-server", "ownedBy-MikeGordievsky"]
+  tags = ["mail-server", "mike-gordievsky"]
 
   boot_disk {
     initialize_params {
